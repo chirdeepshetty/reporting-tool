ec5bc99d4194bc2b7e56a8e7e35a562e298d49ee
unknown <hirenj@.corporate.thoughtworks.com>
2009-07-01 14:19:54 +0530

    Adding the database related files

---
+++ SearchHistory/Models/SearchHistory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using SearchHistory.Infrastructure;
+
+namespace SearchHistory.Models
+{
+    public class SearchHistory
+    {
+        public SearchResult GetHourlySearchData(DateTime fromDate, DateTime toDate)
+        {
+            return new SearchResult();
+        }
+        public SearchResult GetDailySearchData(DateTime fromMonth, DateTime toMonth)
+        {
+            return new SearchResult();
+        }
+        public SearchResult GetIPSearchData()
+        {
+            return new SearchResult();
+        }
+
+
+    }
+
+}
